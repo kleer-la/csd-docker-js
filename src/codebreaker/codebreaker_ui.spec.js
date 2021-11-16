d7234ead07217a000f6af0f7ee2ca652c5d16f10
Juan Gabardini <jgabardini@computer.org>
2021-11-16 00:31:07 -0300

    Juego codebreaker (UI)

---
+++ src/codebreaker/codebreaker_ui.spec.js
@@ -0,0 +1,38 @@
+import fs from "fs";
+
+describe("CodeBreaker", () => {
+    function codigo(nro) {
+        return document.querySelector("#codigo").innerHTML = nro
+    }
+    function adivinar(nro) {
+        document.querySelector("#numero").value = nro;
+        document.querySelector("input[type=submit]").click();
+    }
+    function pistas() {
+        return document.querySelector("#pistas").innerHTML
+    }
+    function resultado() {
+        return document.querySelector("#resultado").innerHTML
+    }
+    beforeAll(() => {
+        document.body.innerHTML = fs.readFileSync("codebreaker.html", "utf8");
+        require("../codebreaker/presenter");
+    })
+    it('Titulo del juego', () => {
+        expect(document.querySelector("title").innerHTML ).toEqual('Codebreaker')
+    })
+    it('Muestra pistas inicial', () => {
+        expect(pistas() ).toEqual('- - - -')
+    })
+    it('Muestra pistas', () => {
+        codigo('3158')
+        adivinar('0123')
+        expect(pistas() ).toEqual('! * - -')
+    })
+    it('Muestra pistas', () => {
+        codigo('3158')
+        adivinar('3158')
+        expect(resultado() ).toEqual('Ganó')
+    })
+
+})
---
+++ src/codebreaker/presenter.js
@@ -0,0 +1,20 @@
+// import Codebreaker from "./codebreaker";
+
+const form = document.querySelector("#codebreaker-form");
+const adivinar = document.querySelector("#numero")
+const pistas = document.querySelector("#pistas");
+const codigo = document.querySelector("#codigo");
+const resultado = document.querySelector("#resultado");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  // const codebreaker = new Codebreaker(codigo.value)
+  // codebreaker.adivinar(adivinar.value)
+  // pistas.innerHTML = codebreaker.pistas
+  pistas.innerHTML = "! * - -"
+
+  // if (codebreaker.gano) {
+    resultado.innerHTML = "Ganó"
+  // }
+});
